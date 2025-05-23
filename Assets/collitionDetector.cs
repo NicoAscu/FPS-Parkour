be10f79a9e88abc9f664e9da39575869811484c9
NicoAscu <nicolasascune123@gmail.com>
2025-05-23 10:40:28 -0300

    .

---
+++ Assets/collitionDetector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collitionDetector : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject);
+    }
+}
